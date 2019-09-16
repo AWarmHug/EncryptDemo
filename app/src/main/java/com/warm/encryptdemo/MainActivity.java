c8bed637b133a57dc0a7f7a8521b6b0519258603
renbin <renbin@chebada.com>
2019-09-16 17:19:57 +0800

    so验证keystore

--- app/src/main/java/com/warm/encryptdemo/MainActivity.java
+++ app/src/main/java/com/warm/encryptdemo/MainActivity.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LookActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = "TAG";
     private String publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAndK6AXnmBKyNsVN0CY+5bOc9uRsPwetdiE4uCKf7l9j7BivfZVyRJQOC+SnaWX9wpp+POXOQ7oQRYXHmoUCYrVYVDFTIe8cc9CZc8HJHXRFibZTkFGD3Lfifdo8LudxPHJSqUju2z/7fNQd3Zj0IQltqi7whaX/WXIccpg2y9yMMQRUywTmrQfRzLiPdPUF+7auoMYVBLlsAsyn/AducB0PGBthYQFxbCWUElE5hUrjEkcBlyz0WPr/NbshbZul4L7bZEiQaib5sa/SjHz50EaOAUf4QmnWDDsxdMaJeDufsrbkQvyQzjrEnRBXPqOhx24GDQdt4G5u4oxU7VZRZOQIDAQAB";
     private String privateKey = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCd0roBeeYErI2xU3QJj7ls5z25Gw/B612ITi4Ip/uX2PsGK99lXJElA4L5KdpZf3Cmn485c5DuhBFhceahQJitVhUMVMh7xxz0JlzwckddEWJtlOQUYPct+J92jwu53E8clKpSO7bP/t81B3dmPQhCW2qLvCFpf9ZchxymDbL3IwxBFTLBOatB9HMuI909QX7tq6gxhUEuWwCzKf8B25wHQ8YG2FhAXFsJZQSUTmFSuMSRwGXLPRY+v81uyFtm6XgvttkSJBqJvmxr9KMfPnQRo4BR/hCadYMOzF0xol4O5+ytuRC/JDOOsSdEFc+o6HHbgYNB23gbm7ijFTtVlFk5AgMBAAECggEASydVu+44QnGI57A9yW/PdLSJB+SDFGzoFUhlU2nqvNxubPnnX3U9CMsrS0tK1uOA2VeR9GG72li5jhWXJaKFEcW0MDMPZSHj/79k8nr40LC+lkAYfDvQeOuzw9lKVC883PdOQ+p7m4Ba+f2cNetKS48MpY3ouzxM77LHSausVVJwzbsgOwnTbH5722DuIY0dwlmDJOxweAKHdA7EL5nRnuRYgUDaQZUwIVPSp/gznB+w9DJP2Kc3c++lquh5RwB2f0tt3bSyryi4xF9vwTy4TIIFNQz7ngwgUKnjcKnQM4Hs6NpnJAIrxSYbVD32NNqB41DNu5SA5NxkElwAq7C9IQKBgQDv1TPT/jTpN/wnLO2pe+4eLEeY5Owv/GzK7ogTlFb8/n4Y9U9hVPHe/4/PdfnU+K4jcjR7FFYAfy7gLexXg+/FUrUggNuOFZOR6UpeKsFeTRoppiozoYlHEyCGC7jWA4vuka4vFfTkLMUPsnVFuFfwGDyBdUK5ylhD1qk/BhuohwKBgQCodkhJHvDzuvFWfUSPvW1e7B5BlIaEVDo1Ds1tLiQ1kgJRWHaxNOtsCczn+pt1adRyoSCnXBYi+oEIVc8NiaWF+nTIkgzDBX8/M6bjf2DaoVfHOW04sKwy0nDxHUW58NRo23lTupCAV/9htHaE7d43XN+stttQtr2kDuYFkOMgPwKBgHcN+iVxWqivPv/fgLcSqzjVOFGAynJjt0nhsTEvASOj3crb86TYexz89lHBOdzr+d3ksoKqrKqgeA+XHGyxNRRaI0nYTaUxtsjG60Dlron2cldcgjSRKslaiFmidgXfStaKANKk7qVaSBSrvSV4BkdMcLS/FfmOpdQ/OlLr4Ez/AoGBAJOzQSV7D19ipH5kTlyIhQAYnV56ToZvIqmqVMpJDk45ufaATXCJKXAMF6+8tdh39WCbjPmvb2kzoTYhpewEpy+UrBe+lZnXk/1h2AzWfaCynTd7ThvKKvBUOdntBFlGobVd2D5k8JTWkn6jrtZVh+OadjIEEL3vus3AkkSHB9wZAoGBAOIQaYIlZD9Vjho9BDkfiAJGwD1jJWRNsusEs5k/lj5o0NU7WaAOTMPHZTwNs8RAk9w71C74Bi70e/7a3NX40MiREBaYKje2HEpshvfwnZAaBShYEoU/3PxvfE+iF6Q3shNW1vnTd2uR+Jur/BZACndXegCJJPLxms8ob9Ildztp";
@@ -24,10 +24,13 @@ public class LookActivity extends AppCompatActivity {
     @BindView(R.id.tv)
     TextView tv;
 
+    @BindView(R.id.tv3)
+    TextView tv3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base64);
+        setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         String strBytes = Arrays.toString(str.getBytes());
         Log.d(TAG, "onCreate: str" + strBytes);
@@ -70,10 +73,12 @@ public class LookActivity extends AppCompatActivity {
 //        Log.d(TAG, "onCreate: publicKey=" + new String(publicKey.getEncoded()));
 //        Log.d(TAG, "onCreate: privateKey=" + new String(privateKey.getEncoded()));
         String encryptRsa = MyRSA.encryptByPublicKey("sjsjskdkjsnkdnjsdnakndsnjds", MyRSA.generatePublic(publicKey));
-        Log.d(TAG, "onCreate: "+encryptRsa);
+        Log.d(TAG, "onCreate: " + encryptRsa);
         String decryptRsa = MyRSA.decryptToStringByPrivateKey(encryptRsa, MyRSA.generatePrivate(privateKey));
 
-        Log.d(TAG, "onCreate: "+decryptRsa);
+        Log.d(TAG, "onCreate: " + decryptRsa);
+
+        tv3.setText(GetSignature.getKey(this));
 
     }
 }
